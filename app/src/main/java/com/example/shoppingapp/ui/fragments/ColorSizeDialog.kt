9e86f5d7520a6e91041a1393cc6674e2d4af164b
hwa <HyunhwaShin@gmail.com>
2021-08-23 01:09:40 +0900

    refactoring[HS-36] ColorSizeDialog.kt
    ColorSizeDialog 의 dialog 나가는 기능 추가, ui 수정 및 xml 이름 수정,

--- app/src/main/java/com/example/shoppingapp/ui/fragments/ColorSizeDialog.kt
+++ app/src/main/java/com/example/shoppingapp/ui/fragments/ColorSizeDialog.kt
@@ -6,28 +6,37 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import com.example.shoppingapp.R
-import com.example.shoppingapp.databinding.FragmentBottonsheetdialogBinding
-import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.example.shoppingapp.databinding.FragmentColorsizedialogBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class ColorSizeDialog : BottomSheetDialogFragment(){
-    private lateinit var binding : FragmentBottonsheetdialogBinding
+    private lateinit var binding : FragmentColorsizedialogBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentBottonsheetdialogBinding.inflate(LayoutInflater.from(context))
+        binding = FragmentColorsizedialogBinding.inflate(LayoutInflater.from(context))
         isCancelable = false
 
         val color = resources.getStringArray(R.array.color_spinner)
         val size = resources.getStringArray(R.array.size_spinner)
         val colorSpinnerAdapter = ArrayAdapter(requireContext(), R.layout.support_simple_spinner_dropdown_item, color)
         val sizeSpinnerAdapter = ArrayAdapter(requireContext(), R.layout.support_simple_spinner_dropdown_item, size)
-        binding.colorSpinner.adapter = colorSpinnerAdapter
-        binding.sizeSpinner.adapter = sizeSpinnerAdapter
+
+        binding.apply {
+            colorSpinner.adapter = colorSpinnerAdapter
+            sizeSpinner.adapter = sizeSpinnerAdapter
+
+            btnExit.setOnClickListener {
+                dismiss()
+            }
+
+        }
+
+
 
         return binding.root
     }
